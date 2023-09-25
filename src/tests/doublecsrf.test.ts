773e6a5ad4a72c141abedd6f96f7ea1db561b1d0
David González Martínez <me@davidgonmar.me>
2023-09-25 17:07:14 +0200

    test: some basic tests for new functionality - wip

--- src/tests/doublecsrf.test.ts
+++ src/tests/doublecsrf.test.ts
@@ -1,13 +1,10 @@
 import { createTestSuite } from "./testsuite.js";
-import { getSecret } from "./utils/helpers.js";
 
-createTestSuite("csrf-csrf unsigned", { getSecret });
+createTestSuite("csrf-csrf unsigned", {});
 createTestSuite("csrf-csrf signed", {
-  getSecret,
   cookieOptions: { signed: true },
 });
 createTestSuite("csrf-csrf signed with custom options", {
-  getSecret,
   cookieOptions: { signed: true, sameSite: "strict" },
   size: 128,
   cookieName: "__Host.test-the-thing.token",
